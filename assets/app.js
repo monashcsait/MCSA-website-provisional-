40ab94d03403ddaabfb6e11b4ff74e2b96b1ef66
Monash Chinese Student Association <monashcsait@gmail.com>
2026-09-19 11:01:09 +1000

    Update app.js

--- assets/app.js
+++ assets/app.js
@@ -100,7 +100,7 @@
       .map(([value, name]) => `<option value="${value}" ${value === lang ? 'selected' : ''}>${name}</option>`).join('');
     const brand = image(data.settings.logo, 'MCSA') + `
       <span>
-        <strong>Monash Chinese Student Association</strong>
+        <strong>MCSA</strong>
         <small>${ui('蒙纳士中国学生会', 'Monash Chinese Students Association', '蒙納士中國學生會')}</small>
       </span>`;
 
