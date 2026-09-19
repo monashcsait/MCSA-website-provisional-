197cb38137251d6f9950bed48872eaabcb3c4727
Monash Chinese Student Association <monashcsait@gmail.com>
2026-09-19 11:10:09 +1000

    Fix typo in association name

--- assets/app.js
+++ assets/app.js
@@ -100,7 +100,7 @@
       .map(([value, name]) => `<option value="${value}" ${value === lang ? 'selected' : ''}>${name}</option>`).join('');
     const brand = image(data.settings.logo, 'MCSA') + `
       <span>
-        <b>Monash Chinses Student Association</b>
+        <b>Monash Chinese Student Association</b>
         <small>${ui('蒙纳士中国学生会', 'Monash Chinese Students Association', '蒙納士中國學生會')}</small>
       </span>`;
 
